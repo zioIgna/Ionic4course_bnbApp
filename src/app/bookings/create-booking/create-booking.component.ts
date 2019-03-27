5f04003257f24b32bcd5763a20d37e238215e85c
unknown <ignaziocarbonaro@hotmail.com>
2019-03-27 20:59:25 +0100

    Aggiunta di un "Modal"

--- src/app/bookings/create-booking/create-booking.component.ts
+++ src/app/bookings/create-booking/create-booking.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Place } from '../../places/place.model';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-create-booking',
@@ -7,8 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateBookingComponent implements OnInit {
 
-  constructor() { }
+  @Input() selectedPlace: Place;
+
+  constructor(private modalCtrl: ModalController) { }
 
   ngOnInit() {}
+
+  onCancel(){
+    this.modalCtrl.dismiss(null, 'cancel');
+  }
+
+  onBookPlace(){
+    this.modalCtrl.dismiss({message: 'This is a dummy message!'}, 'confirm');
+  }
 
 }
--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -36,8 +36,18 @@ export class PlaceDetailPage implements OnInit {
     // this.router.navigateByUrl('/places/tabs/discover');
     // this.navCtrl.navigateBack('/places/tabs/discover');
     // this.navCtrl.pop();
-    this.modalCtrl.create({ component: CreateBookingComponent }).then(modalEl => {
+    this.modalCtrl.create({
+      component: CreateBookingComponent,
+      componentProps: { selectedPlace: this.place }
+    }).then(modalEl => {
       modalEl.present();
+      return modalEl.onDidDismiss();
+
+    }).then(resultData => {
+      console.log(resultData.data, resultData.role);
+      if (resultData.role === 'confirm') {
+        console.log('BOOKED!');
+      }
     });
   }
 
