379c410abf7c517a030a1b1c39a0bb0f9474a701
unknown <ignaziocarbonaro@hotmail.com>
2019-03-27 23:12:43 +0100

    Aggiunto swipe a elementi di una lista

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
+import { IonItemSliding } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-offers',
@@ -9,12 +11,18 @@ import { Place } from '../place.model';
 })
 export class OffersPage implements OnInit {
 
-  constructor(private placesService: PlacesService) { }
+  constructor(private placesService: PlacesService, private router: Router) { }
 
   offers: Place[];
 
   ngOnInit() {
     this.offers = this.placesService.places;
+  }
+
+  onEdit(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerId])
+    console.log('Editing item', offerId);
   }
 
 }
