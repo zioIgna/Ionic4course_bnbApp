610d53fd8c55d77bf693df01491b74f64b5ebcb7
unknown <ignaziocarbonaro@hotmail.com>
2019-03-30 16:00:18 +0100

    Modifiche ai Form Aggiunto controllo validità al form "new-offer" Creato il form "edit-offer"

--- src/app/places/offers/edit-offer/edit-offer.module.ts
+++ src/app/places/offers/edit-offer/edit-offer.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
 import { EditOfferPage } from './edit-offer.page';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {
@@ -17,8 +17,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes)
   ],
   declarations: [EditOfferPage]
