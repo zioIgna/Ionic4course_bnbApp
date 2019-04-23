5be87de9ece7a15c5020e6c77a80ce75fe6bf65c
unknown <ignaziocarbonaro@hotmail.com>
2019-04-23 21:28:51 +0200

    Installazione di Capacitor Servizi di localizzazione automatica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Plugins, Capacitor } from '@capacitor/core';
+// import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+// import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AuthService } from './auth/auth.service';
 import { Router } from '@angular/router';
 
@@ -13,8 +14,8 @@ import { Router } from '@angular/router';
 export class AppComponent {
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
+    // private splashScreen: SplashScreen,
+    // private statusBar: StatusBar,
     private authService: AuthService,
     private router: Router
   ) {
@@ -23,12 +24,15 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      if(Capacitor.isPluginAvailable('SplashScreen')){
+        Plugins.SplashScreen.hide();
+      }
+      // this.statusBar.styleDefault();
+      // this.splashScreen.hide();
     });
   }
 
-  onLogout(){
+  onLogout() {
     this.authService.logout();
     this.router.navigateByUrl('/auth');
   }
