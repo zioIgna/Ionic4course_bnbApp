c9afa90835c1bc6acac5e2f5f6ab873147e6e6fc
unknown <ignaziocarbonaro@hotmail.com>
2019-05-05 14:31:22 +0200

    Modulo Autenticazione gestione centralizzata di signup/login interazione con Firebase anche per autenticazione storage di immagini su Firebase non funziona

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { CanLoad, Route, UrlSegment, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
+import { take, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,10 @@ export class AuthGuard implements CanLoad {
   constructor(private authService: AuthService, private router: Router) { }
 
   canLoad(route: Route, segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    if(!this.authService.userIsAuthenticated){
-      this.router.navigateByUrl('/auth');
-    }
-    return this.authService.userIsAuthenticated;
+    return this.authService.userIsAuthenticated.pipe(take(1), tap(isAuthenticated => {
+      if (!isAuthenticated) {
+        this.router.navigateByUrl('/auth');
+      }
+    }));
   }
 }
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -138,34 +138,39 @@ export class PlacesService {
     imageUrl: string
   ) {
     let generatedId: string;
-    const newPlace = new Place(
-      Math.random().toString(),
-      title,
-      description,
-      imageUrl,
-      price,
-      dateFrom,
-      dateTo,
-      this.authService.userId,
-      location
-    );
-    return this.http
-      .post<{ name: string }>('https://ionic-angular-course-fb3d1.firebaseio.com/offered-places.json',
-        { ...newPlace, id: null })
-      .pipe(
-        switchMap(
-          resData => {
-          console.log('questo è resData: ', registerLocaleData);
-            generatedId = resData.name;
-            return this.places;
-          }
-        ),
-        take(1),
-        tap(places => {
-          newPlace.id = generatedId;
-          this._places.next(places.concat(newPlace));
-        })
+    let newPlace: Place;
+    return this.authService.userId.pipe(take(1), switchMap(userId => {
+      if (!userId) {
+        throw new Error('No user found!');
+      }
+      newPlace = new Place(
+        Math.random().toString(),
+        title,
+        description,
+        imageUrl,
+        price,
+        dateFrom,
+        dateTo,
+        userId,
+        location
       );
+      return this.http
+        .post<{ name: string }>('https://ionic-angular-course-fb3d1.firebaseio.com/offered-places.json',
+          { ...newPlace, id: null })
+    }),
+      switchMap(
+        resData => {
+          console.log('questo è resData: ', registerLocaleData);
+          generatedId = resData.name;
+          return this.places;
+        }
+      ),
+      take(1),
+      tap(places => {
+        newPlace.id = generatedId;
+        this._places.next(places.concat(newPlace));
+      })
+    );
     // return this.places.pipe(
     //   take(1),
     //   delay(1000),
